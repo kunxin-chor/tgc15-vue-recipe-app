bd5ea60e16ac03d2eb49378e1cad21d5ea10ad84
kunxin-chor <chorkunxin@yahoo.com>
2022-01-03 12:31:12 +0000

    Show all recipes and WIP for creating a add new recipe form

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>Add Recipe</h1>
+    <div>
+        <h1>Add Recipe</h1>
+        <div>
+            <label>Title</label>
+            <input type="text" v-model="title"/>
+        </div>
+        <div>
+            <label>Ingredients</label>
+            <input type="text" 
+                   placeholder="Seperate each ingredient with a comma"
+                   v-model="ingredients"/>
+        </div>
+        <button v-on:click="addNew">Add New</button>
+    </div>
 </template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            title: "",
+            ingredients:""
+        }
+    }
+}
+</script>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>All Recipes</h1>
+    <div>
+        <h1>All Recipes</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="r in recipes" v-bind:key="r._id">
+                    <td>{{r._id}}</td>
+                    <td>{{r.title}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
 </template>
+
+<script>
+
+const BASE_API_URL="https://3000-blush-donkey-bpbrtfz9.ws-us25.gitpod.io/"
+
+import axios from 'axios'
+export default {
+    created:async function() {
+        let response = await axios.get(BASE_API_URL + 'recipes');
+        this.recipes = response.data
+    },
+    data:function(){
+        return {
+            'recipes':[]
+        }
+    }
+}
+</script>
